3e20b8808de76cadf5808f55383082f4e6a5df56
Lesther Villatoro <rodnyvillatorotzoc@gmail.com>
2025-07-30 00:47:23 -0600

    Función configurar_expresion y menú
    Se creó la función configurar_expresion para ir convirtiendo los operadores y se creó el menú para que esté más ordenado

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-expr = input("Ingrese expresión lógica (&&, ||, !, ->, <->): ").strip()
 
 # Mapeo de operadores a Python
 mapeado = {
@@ -9,6 +8,28 @@ mapeado = {
     '!'  : ' not ',
 }
 
-expr_py = expr
-for simb, py in mapeado.items():
-    expr_py = expr_py.replace(simb, py)
+operadores = ['<->', '->', '&&', '||', '!']
+
+def configurar_expresion():
+    expr = input("Ingrese expresión lógica (&&, ||, !, ->, <->): ").strip()
+    # Reemplazar símbolos en el orden definido
+    expr_py = expr
+    for simb in operadores:
+        if simb in expr_py:
+            expr_py = expr_py.replace(simb, mapeado[simb])
+            
+#Menú           
+def menu():
+    configurar_expresion()
+    while True:
+        opcion = input("\n¿Generar tabla de verdad con esta expresión? (S/N) ").strip().lower()
+        if opcion == 's':
+            break
+        elif opcion == 'n':
+            configurar_expresion()
+        else:
+            print("Opción incorrecta, ingrese S o N.")
+
+# Ejecutar menú
+if __name__ == '__main__':
+    menu()
